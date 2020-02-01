275bea2774f917c818b6b258ba3ac8173e44cfa9
Sid Naik <sid.naik27@gmail.com>
2020-02-01 17:07:51 -0500

    andrew's new Train setup

--- Assets/SpawnHole.cs
+++ Assets/SpawnHole.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SpawnHole : MonoBehaviour
 {
-    public Hole hole;
+    public GameObject hole;
+    public HoleManager hm;
     // Start is called before the first frame update
     void Start()
     {
