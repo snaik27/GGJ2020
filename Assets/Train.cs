8f58ff2b08090b8cee9d58fe78f126e093fb76a5
Sid Naik <sid.naik27@gmail.com>
2020-02-01 16:19:42 -0500

    Trains setup

---
+++ Assets/Train.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    [SerializeField] private Vector3 OriginalWorldPos;
+    [SerializeField] public float trainSpeed;
+    [SerializeField] public bool canMove;
+
+    private void Awake()
+    {
+        OriginalWorldPos = transform.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            transform.Translate(transform.right * trainSpeed);
+        }
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = OriginalWorldPos;
+    }
+}
---
+++ Assets/TrainTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Train"))
+        {
+            other.GetComponent<Train>().ResetPosition();
+            if(other.GetComponent<Train>().trainSpeed < 0)
+            {
+                other.GetComponent<Train>().trainSpeed = (int)Random.Range(-5.0f, -1.0f);
+            }
+            else
+            {
+                other.GetComponent<Train>().trainSpeed = (int)Random.Range(1.0f, 5.0f);
+
+            }
+
+        }
+    }
+}
