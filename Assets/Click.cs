da610d8e61cefa4367404b0e6a93b65fcf927f29
a-hsu <a.hsu873@gmail.com>
2020-02-01 15:21:20 -0500

    textmeshpro, more basic scripts

---
+++ Assets/Click.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class Click : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+            Debug.Log("Pressed");
+        */
+    }
+
+    public void OnAKey()
+    {
+        Debug.Log("Jumped");
+        score++;
+        string txt = score.ToString();
+        text.text = txt;
+    }
+}
---
+++ Assets/GameManagerSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSingleton : MonoBehaviour
+{
+    public static GameManagerSingleton instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
+    private int level = 1;
+    //private Level level;
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Get a component reference to the attached BoardManager script
+     //   boardScript = GetComponent<BoardManager>();
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        
+    }
+}
+
+
