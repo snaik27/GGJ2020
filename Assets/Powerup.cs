814455f991e69c239adf396c1497b78069313d91
Sid Naik <sid.naik27@gmail.com>
2020-02-02 14:01:17 -0500

    Fixed shovel, powerup stuff

--- Assets/Powerup.cs
+++ Assets/Powerup.cs
@@ -6,7 +6,7 @@ using System.Linq;
 public class Powerup : MonoBehaviour
 {
     public List<Transform> SpawnLocations;
-
+    public Material myMaterial;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
@@ -14,5 +14,12 @@ public class Powerup : MonoBehaviour
             int randal = (int)Random.Range(0f, SpawnLocations.Count);
             transform.position = new Vector3(SpawnLocations[randal].position.x, 1f, SpawnLocations[randal].position.z);
         }
+    }
+
+
+    private void Update()
+    {
+        float emissionVal = Mathf.Lerp(0f, 0.5f, Mathf.Sin(10f * Time.time) * 0.5f + 0.5f);
+        myMaterial.SetFloat("_EmissionValue", emissionVal);
     }
 }
